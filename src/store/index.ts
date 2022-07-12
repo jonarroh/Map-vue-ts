290cbb12bcfadfe01bbcdc24354051930872278c
jonarroh <jonarrodi99@gmail.com>
2022-07-12 12:56:29 -0500

    first commit

---
+++ src/store/index.ts
@@ -0,0 +1,17 @@
+import { createStore } from 'vuex';
+
+// My custom modules
+import exampleModule from './module-template';
+import { ExampleStateInterface } from './module-template/state';
+
+
+export interface StateInterface {
+
+  example: ExampleStateInterface
+}
+
+export default createStore<StateInterface>({
+  modules: {
+    example: exampleModule
+  }
+})
