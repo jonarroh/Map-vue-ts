d9eea6aa45d1587a4cc4f7606aa37a55ef5a0b63
jonarroh <jonarrodi99@gmail.com>
2022-07-17 16:23:28 -0500

    🛠️ se agrego la api de busqueda

--- src/store/places/mutations.ts
+++ src/store/places/mutations.ts
@@ -1,10 +1,18 @@
 import { MutationTree } from 'vuex';
+import { Feature } from '../../interfaces/places';
 import { PlacesState } from './state';
 
 const mutation: MutationTree<PlacesState> = {
 	setUserLocation(state, payload: [number, number]) {
 		state.userLocation = payload;
 		state.isLoading = false;
+	},
+	setPlaces(state, payload: Feature[]) {
+		state.places = payload;
+		state.isLoadingPlaces = false;
+	},
+	setIsLoadingPlaces(state) {
+		state.isLoadingPlaces = true;
 	}
 };
 
