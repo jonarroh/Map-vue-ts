b0563d3bfe77d5f8a61cdf78c4554d2846b7be96
jonarroh <jonarrodi99@gmail.com>
2022-07-19 17:01:14 -0500

    🛠️ added polylines

--- src/composables/useMapStore.ts
+++ src/composables/useMapStore.ts
@@ -2,6 +2,7 @@ import { useStore } from 'vuex';
 import { computed } from 'vue';
 import { StateInterface } from '../store';
 import { Feature } from '../interfaces/places';
+import { LngLat } from '../store/map/actions';
 
 export const useMapStore = () => {
 	const store = useStore<StateInterface>();
@@ -19,6 +20,11 @@ export const useMapStore = () => {
 		},
 		setPlacesMarkers: (places: Feature[]) => {
 			store.commit('map/setPlacesMarkers', places);
+		},
+
+		//actions
+		getRouteBetweenPoints: (start: LngLat, end: LngLat) => {
+			store.dispatch('map/getRouteBetweenPoints', { start, end });
 		}
 	};
 };
